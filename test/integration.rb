09259075b138729a12fdb754bd6d8c0f38364682
Yasuhiro Asaka <y.grauwoelfchen@gmail.com>
2011-12-29 18:52:35 +0900

    added part of offest support commit by johanlunds.
    - No change in api.
    - added offset support for Excel Worksheet.
    
    original message is:
    Add support for saving merged cells when editing *existing* files.
    
    API is:
    
      * `worksheet.merged_cells = [[start_row, end_row, start_col, end_col], ...]`
      * `worksheet.merged_cells`
    
    This is a continuation of "Add merged cells functionality" (e5906f3) in Github-fork timon/spreadsheet.
    
    PLEASE NOTE: this is totally untested and probably breaks other functionality. It's currently just a quick hack for my own needs. Should be code reviewed by someone knowledgeable, and have tests written for it.

--- test/integration.rb
+++ test/integration.rb
@@ -787,6 +787,7 @@ module Spreadsheet
       book = Spreadsheet::Workbook.new
       sheet = book.create_worksheet :name => 'merged cells'
       sheet[3, 0] = 'merge'
+      assert sheet.merged_cells.empty?
       sheet.merge_cells *[0, 0, 0, 1]
       sheet.merge_cells *[1, 0, 2, 0]
       assert_equal [0, 0, 0, 1], sheet.merged_cells[0]
@@ -801,6 +802,8 @@ module Spreadsheet
       assert_equal 2, sheet.merged_cells.size
       assert_equal [0, 0, 0, 1], sheet.merged_cells.shift
       assert_equal [1, 2, 0, 0], sheet.merged_cells.shift
+      assert sheet.offsets.has_key?(:merged_cells)
+      assert_equal 2, sheet.offsets[:merged_cells].size
     end
     def test_merge_cells_in_existing_file
       path = File.join @data, 'test_version_excel97.xls'
@@ -810,6 +813,7 @@ module Spreadsheet
       assert_equal 'Microsoft Excel 97/2000/XP', book.version_string
       sheet = book.create_worksheet :name => 'merged cells'
       sheet[3, 0] = 'merge'
+      assert sheet.merged_cells.empty?
       sheet.merge_cells *[0, 0, 0, 1]
       sheet.merge_cells *[1, 0, 2, 0]
       assert_equal [0, 0, 0, 1], sheet.merged_cells[0]
@@ -824,6 +828,8 @@ module Spreadsheet
       assert_equal 2, sheet.merged_cells.size
       assert_equal [0, 0, 0, 1], sheet.merged_cells.shift
       assert_equal [1, 2, 0, 0], sheet.merged_cells.shift
+      assert sheet.offsets.has_key?(:merged_cells)
+      assert_equal 2, sheet.offsets[:merged_cells].size
     end
     def test_write_to_stringio
       book = Spreadsheet::Excel::Workbook.new
