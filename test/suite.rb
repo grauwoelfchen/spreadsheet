858f21c2689325ba6f696f9b215d414a6525055e
Yasuhiro Asaka <y.grauwoelfchen@gmail.com>
2012-01-01 23:32:59 +0900

    fixed load_path problem and run on major rubies.
    - without oniguruma.
    - tested 1.8.6, 1.8.7, 1.9.2, 1.9.3
    - fixed load_path support (it dosen't current path) on ruby over 1.9.2

--- test/suite.rb
+++ test/suite.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/env ruby
 # suite.rb -- spreadsheet -- 26.07.2011 -- zdavatz@ywesee.com
 
+require 'rubygems' if RUBY_VERSION < '1.9'
 require 'find'
 
 here = File.dirname(__FILE__)
 
 $: << here
 
-Find.find(here) do |file|
-	if /(?<!suite)\.rb$/o.match(file)
+Find.find(here) do |path|
+#  if /(?<!suite)\.rb$/o.match(file)
     #from Roel van der Hoorn vanderhoorn@gmail.com
     #should work for Ruby 1.8 and 1.9, without Oniguruma
 #  if /(?:^|\/)(?!suite)[^\/]+\.rb$/o
-    require file
+  next if File.directory?(path)
+  if path !~ /suite\.rb$/o && path =~ /\.rb$/o
+    require File.expand_path(path)
 	end
 end
